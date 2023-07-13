fe9ae418e20db112666a483d53b31b0a6f11427a
Stranix <Phantom2525@gmail.com>
2023-07-13 13:30:34 +0300

    refactor code in tg_bot module

--- tg_bot.py
+++ tg_bot.py
@@ -13,8 +13,8 @@ from telegram.ext import (
 )
 
 from dialogflow import detect_intent_texts
+from logging_handlers import TelegramLogsHandler
 
-load_dotenv()
 logger = logging.getLogger('recognize_speech_bot')
 
 
@@ -25,37 +25,50 @@ def start(update: Update, context: CallbackContext):
     )
 
 
-def echo(update: Update, context: CallbackContext):
-    dialog_flow_project_id = os.environ['DIALOGFLOW_PROJECT_ID']
+def send_replay_message(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     _, message = detect_intent_texts(
-        dialog_flow_project_id,
+        context.bot_data["dialogflow_project_id"],
         str(chat_id),
         update.message.text
     )
     context.bot.send_message(chat_id, message)
 
 
+def run_tg_bot(tg_token: str, dialogflow_project_id: str):
+    logger.info('Старт бота')
+    updater = Updater(token=tg_token, use_context=True)
+    dispatcher = updater.dispatcher
+    dispatcher.bot_data["dialogflow_project_id"] = dialogflow_project_id
+
+    start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(
+        Filters.text & (~Filters.command),
+        send_replay_message
+    )
+
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(echo_handler)
+
+    updater.start_polling()
+    updater.idle()
+
+
 if __name__ == '__main__':
     try:
-        logging.basicConfig(
-            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-            level=logging.DEBUG
-        )
-        logger.info('Старт бота')
+        load_dotenv()
+        tg_bot_token = os.environ['TG_BOT_TOKEN']
+        tg_admin_id = int(os.environ['TG_ADMIN_ID'])
+        dialog_flow_project_id = os.environ['DIALOGFLOW_PROJECT_ID']
 
-        tg_bot_api = os.environ['TG_BOT_API']
-        updater = Updater(token=tg_bot_api, use_context=True)
-        dispatcher = updater.dispatcher
+        logging.basicConfig(level=logging.ERROR)
+        logger.addHandler(TelegramLogsHandler(tg_bot_token, tg_admin_id))
 
-        start_handler = CommandHandler('start', start)
-        echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-
-        dispatcher.add_handler(start_handler)
-        dispatcher.add_handler(echo_handler)
-
-        updater.start_polling()
+        run_tg_bot(tg_bot_token, dialog_flow_project_id)
     except KeyError:
         logger.critical('Заданы не все переменные окружения')
     except KeyboardInterrupt:
-        logger.info('Работа программы завершена')
+        logger.warning('Работа программы завершена')
+    except Exception as error:
+        logger.exception(error)
+
